dc49f44b1adb44a0515e8122a9a032b47cda61db
wisnu kurniawan <wisnuko13@gmail.com>
2020-08-15 12:38:57 +0700

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-pg-1
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ src/clj_pg_1/core.clj
@@ -0,0 +1,9 @@
+(ns clj-pg-1.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
+
+(+ 1 2)
+(+ 2 3)
